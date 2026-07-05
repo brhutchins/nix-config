11cbcf2439fb17295a4903f01c10ba58c8029b51
brhutchins <bhutchins@gmail.com>
2026-07-05 14:17:09 +0200

    chore(darwin): revert to upstream behaviour for brew cleanup

--- modules/mk-darwin.nix
+++ modules/mk-darwin.nix
@@ -1,30 +1,20 @@
 { inputs }:
 let
   system = "aarch64-darwin";
-  pkgs = import inputs.nixpkgs { inherit system; };
-  nix-darwin-src = pkgs.runCommand "nix-darwin-patched" { } ''
-    cp -R ${inputs.nix-darwin} "$out"
-    chmod -R u+w "$out"
-    substituteInPlace "$out/modules/homebrew.nix" \
-      --replace-fail '++ optional (config.cleanup == "uninstall") "--force-cleanup"' '++ optional (config.cleanup == "uninstall") "--cleanup"' \
-      --replace-fail '++ optional (config.cleanup == "zap") "--zap --force-cleanup"' '++ optional (config.cleanup == "zap") "--cleanup --zap"'
-  '';
-  nix-darwin-patched = pkgs.applyPatches {
-    name = "nix-darwin-patched";
-    src = nix-darwin-src;
-    patches = [ ];
-  };
-  nix-darwin = pkgs.lib.fix (self:
-    (import "${nix-darwin-patched}/flake.nix").outputs {
-      inherit self;
-      nixpkgs = inputs.nixpkgs;
-    });
+  inherit (inputs) nix-darwin;
   data = import ./data;
-in {
-  mkHost = { profile, home, hostConfig }:
+in
+{
+  mkHost =
+    {
+      profile,
+      home,
+      hostConfig,
+    }:
     let
       username = data.username;
-    in nix-darwin.lib.darwinSystem {
+    in
+    nix-darwin.lib.darwinSystem {
       specialArgs = { inherit inputs system username; };
       modules = [
         ./overlays.nix
